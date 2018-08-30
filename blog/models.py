5f811cc8688f19244b2e976333d0742173d2ad0c
Harrison <harrisonekpobimi@gmail.com>
2018-08-30 11:49:02 +0200

    blog models addition

--- blog/models.py
+++ blog/models.py
@@ -4,3 +4,9 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+class Blog(models.Model):
+	title = models.CharField(max_length=355)
+	publishedDate = models.DateTimeField()
+	body = models.TextField()
+	image = models.ImageField(upload_to='images/')
