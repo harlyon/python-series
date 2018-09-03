da3464a851f7144dccc00f9f63b8aae8a8a952ea
Harrison <harrisonekpobimi@gmail.com>
2018-09-03 15:09:36 +0200

    changes

--- blog/models.py
+++ blog/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Blog(models.Model):
     title = models.CharField(max_length=255)
-    pub_date = models.DateTimeField()
+    publishedDate = models.DateTimeField()
     body = models.TextField()
     image = models.ImageField(upload_to='images/')
 
@@ -13,4 +13,4 @@ class Blog(models.Model):
         return self.body[:100]
 
     def pub_date_pretty(self):
-        return self.pub_date.strftime('%b %e %Y')
+        return self.publishedDate.strftime('%b %e %Y')
